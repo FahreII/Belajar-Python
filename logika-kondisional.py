7fbe866d1046d4793fe1621ead8c5e0b25d1fccc
Abitien Ahmad Fahrel <abitienfahrel@gmail.com>
2025-10-10 10:00:53 +0700

    update

--- logika-kondisional.py
+++ logika-kondisional.py
@@ -12,3 +12,15 @@ elif nilai > 50:
 else:
     print ("nilai D")
 
+nilai_MK_PD = "A"
+kehadiran=100
+
+if nilai_MK_PD == "A" and kehadiran ==100:
+    print("dapat")
+else:
+    print("tidak")
+
+if nilai_MK_PD == "A" or kehadiran ==100:
+    print("dapat")
+else:
+    print("tidak")
