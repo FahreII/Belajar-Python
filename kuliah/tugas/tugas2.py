aabd9bd80697b81a46a7656fff8df58fa9e6945b
Abitien Ahmad Fahrel <abitienfahrel@gmail.com>
2025-10-24 15:23:24 +0700

    update tugas

--- kuliah/tugas/tugas2.py
+++ kuliah/tugas/tugas2.py
@@ -1,8 +1,21 @@
 
-nama = input("Masukan nama mahasisawa : ")
-jumlah_mk = int(input("Masukan jumlah matkul : "))
-
 total_nilai = 0
 
-for i in range(jumlah_mk):
-    print()
+nama = str(input("Masukan nama mahasiswa : "))
+jumlah_mk = int(input("Masukan jumlah mata kuliah : "))
+for i in range (jumlah_mk):
+    nama_mk = str(input("Masukan nama mata kuliah : "))
+    nilai = float(input("Masukan nilai (0-100) : "))
+
+    while nilai < 0 or nilai > 100:
+        print("Nilai harus antara 0-100!")
+        nilai = float(input("Masukan nilai 0 - 100 : "))
+    total_nilai += nilai
+
+ipk = total_nilai / jumlah_mk
+if ipk > 60:
+    status = "LULUS"
+else:
+    status = "TIDAK LULUS"
+print(f"Nama mahasiswa : {nama}")
+print(f"Status         : {status}")
